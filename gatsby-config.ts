9df2b2e7b884009ddb9891ceb03e7e90f96cfbad
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-12-14 16:52:54 +0900

    Images now show up. The path in the markdown files for the images has to be relative. The best thing might be to keep everything in the gatsby project in a content folder.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,14 +10,30 @@ const config: GatsbyConfig = {
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
     plugins: [
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                name: `content`,
                 path: `/Users/hjalte/Library/CloudStorage/OneDrive-Personal/Obsidian Vault/Bloggen`,
             },
         },
-        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        },
+                    },
+                ],
+            },
+        },
     ],
 }
 
