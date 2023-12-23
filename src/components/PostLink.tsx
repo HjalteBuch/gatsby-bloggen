c4512d2ef985554c8c9a957746574f1eb9d2158b
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-12-23 16:22:38 +0900

    Finally centered text on images.

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 const PostLink = ({ post }) => (
   <div>
     <Link className="article" to={"/blog/" + post.frontmatter.slug}>
-        <img 
-            className="image"
-            src={post.frontmatter.image01.publicURL} 
-            alt={post.frontmatter.title}
-        ></img>
-        <h1 className="header">{post.frontmatter.title} ({post.frontmatter.date})</h1>
+        <div className="image-container">
+            <img 
+                className="image"
+                src={post.frontmatter.image01.publicURL} 
+                alt={post.frontmatter.title}
+            ></img>
+            <h1 className="header">{post.frontmatter.title}</h1>
+        </div>
     </Link>
     <p>description: {post.frontmatter.description}</p>
   </div>
